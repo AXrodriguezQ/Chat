1128f38d20cf2a261ea70c6a1f8bc696bdca6e66
AXrodriguezQ <alexanderrodriguez3223@gmail.com>
2025-08-11 20:32:41 -0500

    server config

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 import express from "express";
+import morgan from "morgan";
+import { Server as SocketServer } from "socket.io";
+import http from 'http'
+import cors from 'cors'
+import { PORT } from "./config.js";
 
 const app = express()
-const PORT = 3002
+const server = http.createServer(app)
+const io = new SocketServer(server)
+
+app.use(cors())
+app.use(morgan('dev'))
 
 app.listen(PORT)
 console.log('Application running in port', PORT)
